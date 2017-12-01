6167108fabbada67662ee765e2d9765efce1352d
Spencer Herzberg <spencer.herzberg@gmail.com>
2017-12-01 10:33:57 -0600

    latest

--- examples/ruby/run.rb
+++ examples/ruby/run.rb
@@ -1,6 +1,8 @@
 require 'sinatra'
 require 'gelf'
 
+VERSION = '0.0.9'
+
 set :bind, '0.0.0.0'
 
 get '/' do
@@ -9,11 +11,24 @@ get '/' do
 
   logger.info "hello world"
 
+  logger.add GELF::INFO, {
+      :short_message => 'random_counter',
+      :random_counter_value => Random.rand(11),
+      :app_version => VERSION
+  }
+
   'Hello world!'
 end
 
 get '/status' do
   logger = GELF::Logger.new("graylog", 12201, "WAN", { :facility => "graylog-talk-example-ruby" })
   logger.debug 'status was called'
+
+  logger.add GELF::INFO, {
+      :short_message => 'random_counter',
+      :random_counter_value => Random.rand(11),
+      :app_version => VERSION
+  }
+
   'OK'
 end
